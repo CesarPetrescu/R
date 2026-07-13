69eca848c5488813cb41067338c73dbe943f9bb8
r-coder <r-coder@users.noreply.github.com>
2026-07-13 06:35:25 +0300

    feat: add Rustic helm balance helpers

--- runtime/rustic.c
+++ runtime/rustic.c
@@ -1949,6 +1949,10 @@ static struct Value parse_factor(struct Parser *parser) {
             int measuring_signal_density_band_span_gap_delta_balance_raft = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_raft") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_raft") == 0;
             int measuring_signal_density_band_span_gap_delta_balance_sail = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_sail") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_sail") == 0;
             int measuring_signal_density_band_span_gap_delta_balance_mast = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_mast") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_mast") == 0;
+            int measuring_signal_density_band_span_gap_delta_balance_helm = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_helm") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_helm") == 0;
+            if (measuring_signal_density_band_span_gap_delta_balance_helm) {
+                measuring_signal_density_band_span_gap_delta_balance_mast = 1;
+            }
             if (measuring_signal_density_band_span_gap_delta_balance_mast) {
                 measuring_signal_density_band_span_gap_delta_balance_sail = 1;
             }
@@ -2941,6 +2945,9 @@ static struct Value parse_factor(struct Parser *parser) {
                     }
                     if (measuring_signal_density_band_span_gap_delta_balance_mast && matching_mass > 1 && shortest_run > 0) {
                         matched -= matching_mass + shortest_run;
+                    }
+                    if (measuring_signal_density_band_span_gap_delta_balance_helm && matching_mass > 1) {
+                        matched -= matching_mass + transition_count;
                     }
                     compact_unreferenced_arrays(parser, &arguments[0]);
                     return parse_index_postfix(parser, integer_value(matched));
