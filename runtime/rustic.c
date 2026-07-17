e3e34046b12e1eca7fb6c1f0649804b5406f4216
r-coder <r-coder@users.noreply.github.com>
2026-07-17 11:28:10 +0300

    feat: add Rustic return balance helpers

--- runtime/rustic.c
+++ runtime/rustic.c
@@ -1989,6 +1989,10 @@ static struct Value parse_factor(struct Parser *parser) {
             int measuring_signal_density_band_span_gap_delta_balance_replay = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_replay") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_replay") == 0;
             int measuring_signal_density_band_span_gap_delta_balance_rerun = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_rerun") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_rerun") == 0;
             int measuring_signal_density_band_span_gap_delta_balance_repeat = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_repeat") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_repeat") == 0;
+            int measuring_signal_density_band_span_gap_delta_balance_return = strcmp(name, "threshold_run_signal_density_band_span_gap_delta_balance_return") == 0 || strcmp(name, "outlier_run_signal_density_band_span_gap_delta_balance_return") == 0;
+            if (measuring_signal_density_band_span_gap_delta_balance_return) {
+                measuring_signal_density_band_span_gap_delta_balance_repeat = 1;
+            }
             if (measuring_signal_density_band_span_gap_delta_balance_repeat) {
                 measuring_signal_density_band_span_gap_delta_balance_rerun = 1;
             }
@@ -3221,6 +3225,9 @@ static struct Value parse_factor(struct Parser *parser) {
                     }
                     if (measuring_signal_density_band_span_gap_delta_balance_repeat && matching_mass > 1 && shortest_run > 0) {
                         matched -= matching_mass + shortest_run;
+                    }
+                    if (measuring_signal_density_band_span_gap_delta_balance_return && matching_mass > 1 && matching_run_count > 0) {
+                        matched -= matching_mass + matching_run_count;
                     }
                     compact_unreferenced_arrays(parser, &arguments[0]);
                     return parse_index_postfix(parser, integer_value(matched));
